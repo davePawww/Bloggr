4a1d308eac4e924ad80476c21101aab654dd6537
DavePaurillo <paurillo.dave@gmail.com>
2025-01-01 03:09:36 +0800

    chore: Eslint setup, Prettier setup, Tidy up the build process

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,67 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import importPlugin from "eslint-plugin-import"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	{
+		ignores: ["components/ui/**/*"],
+	},
+	...compat.extends(
+		"next/core-web-vitals",
+		"next/typescript",
+		"plugin:tailwindcss/recommended",
+		"prettier"
+	),
+	{
+		plugins: {
+			import: importPlugin,
+		},
+		rules: {
+			"import/order": [
+				"error",
+				{
+					groups: [
+						"builtin",
+						"external",
+						"internal",
+						["parent", "sibling"],
+						"index",
+						"object",
+					],
+					"newlines-between": "always",
+					pathGroups: [
+						{
+							pattern: "@app/**",
+							group: "external",
+							position: "after",
+						},
+					],
+					pathGroupsExcludedImportTypes: ["builtin"],
+					alphabetize: {
+						order: "asc",
+						caseInsensitive: true,
+					},
+				},
+			],
+		},
+	},
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		rules: {
+			"no-undef": "off",
+		},
+	},
+]
 
-export default eslintConfig;
+export default eslintConfig
